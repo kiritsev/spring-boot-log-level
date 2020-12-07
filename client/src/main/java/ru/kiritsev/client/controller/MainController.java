d4181dc55ea7a8749d9374817096d273eda6f4a3
Petr Kiritsev <ppkiritsev@alfabank.ru>
2020-12-07 20:44:59 +0300

    initial commit

---
+++ client/src/main/java/ru/kiritsev/client/controller/MainController.java
@@ -0,0 +1,23 @@
+package ru.kiritsev.client.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    private Log log = LogFactory.getLog(MainController.class);
+
+    @GetMapping("/hello")
+    String sayHello() {
+        log.trace("This is a TRACE level message");
+        log.debug("This is a DEBUG level message");
+        log.info("This is an INFO level message");
+        log.warn("This is a WARN level message");
+        log.error("This is an ERROR level message");
+        return "hello!";
+    }
+
+}
